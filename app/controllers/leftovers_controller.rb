731472a96eaa52b3addbc575948d91d237a0c107
Денис Пышкин <avatar48@yandex.ru>
2018-03-30 14:28:31 +0300

    fix дата в остатках

--- app/controllers/leftovers_controller.rb
+++ app/controllers/leftovers_controller.rb
@@ -1,13 +1,13 @@
 class LeftoversController < ApplicationController
   before_filter :authenticate_user! 
 	def send_leftovers
-		datato1c = Time.new(params[:start_date][:year].to_i,params[:start_date][:month].to_i,params[:start_date][:day].to_i).strftime("%Y-%m-%d")
+		date = Time.new(params[:start_date][:year].to_i,params[:start_date][:month].to_i,params[:start_date][:day].to_i)
 		client = Savon.client(wsdl: ENV['WSDL'], basic_auth: [ENV['USN'], ENV['PAS']])
-		x = client.call(:get, message: {date: datato1c}).to_hash
+		x = client.call(:get, message: {date: date.strftime("%Y-%m-%d")}).to_hash
 		result = x[:get_response][:return][:НоменклатураСписка]
-	 	data = "#{params[:start_date][:day].to_i}.#{params[:start_date][:month].to_i}.#{params[:start_date][:year].to_i} 23:59:59"
-		builder = Nokogiri::XML::Builder.new(:encoding => 'UTF-8') { |xml|
-		xml.Шапка('Составил' => '', 'ДатаСоставления' => "#{Time.now.strftime('%d.%m.%y %H:%M:%S')}", "ДатаСреза" => "#{data}") {
+	# byebug
+		 builder = Nokogiri::XML::Builder.new(:encoding => 'UTF-8') { |xml|
+		xml.Шапка('Составил' => '', 'ДатаСоставления' => "#{Time.now.strftime('%d.%m.%y %H:%M:%S')}", "ДатаСреза" => "#{date.strftime("%d.%m.%y")} 23:59:59") {
 			xml.Склад('Наименование' => 'Склад КЛУ'){
 				result.map do |row|
 
@@ -18,6 +18,6 @@ class LeftoversController < ApplicationController
 		}
 	}
 	file = builder.to_xml
-	send_data file, :type => 'text/xml; charset=UTF-8;', :disposition => "attachment; filename=Остатки на #{data}.xml"
+	send_data file, :type => 'text/xml; charset=UTF-8;', :disposition => "attachment; filename=Остатки на #{date}.xml"
   end
 end
